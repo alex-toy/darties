18591f39b1af5a19eec419fbd1fd95f5fdae4e0d
alessio rea <alexei.80@hotmail.fr>
2021-04-23 17:41:39 +0200

    progress load fact table

--- airflow/plugins/helpers/sql_queries.py
+++ airflow/plugins/helpers/sql_queries.py
@@ -27,25 +27,6 @@ class SqlQueries:
         WHERE page='NextSong'
     """)
 
-    song_table_insert = ("""
-        SELECT distinct song_id, title, artist_id, year, duration
-        FROM staging_songs
-    """)
-
-    artist_table_insert = ("""
-        SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
-        FROM staging_songs
-    """)
-
-    time_table_insert = ("""
-        SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
-               extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
-        FROM songplays
-    """)
-
-
-
-
 
 
 
@@ -74,8 +55,8 @@ class SqlQueries:
         JOIN temps 
             ON staging_v_{0}.annee = temps.annee
             AND temps.mois = '{1}'
-        JOIN famille_produit
-        JOIN magasin
+        JOIN famille_produit ON ???
+        JOIN magasin ON ???
     """) # to be finished!!!
 
     sales_table_insert.format('fours', 'janvier')

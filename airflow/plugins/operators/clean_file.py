7dc3468d3b25cf82dca7eb788a23d731d78eba73
alessio rea <alexei.80@hotmail.fr>
2021-04-24 08:19:57 +0200

    progress automating year retrieval

--- airflow/plugins/operators/clean_file.py
+++ airflow/plugins/operators/clean_file.py
@@ -5,6 +5,7 @@ import os
 import config.config as cf
 from os import listdir
 from os.path import isfile, join
+import re
 
 
 from infrastructure.SalesData import SalesData
@@ -26,12 +27,12 @@ class CleanFileOperator(BaseOperator) :
         self.log.info(f"Create cleaned files in : {cf.DATA_DIR}")
         files = [f for f in listdir(cf.DATA_DIR) if isfile(join(cf.DATA_DIR, f))]
         for file in files :
-            file_abspath = os.path.abspath(os.path.join(dirpath, file))
-            self.log.info(f"file_abspath : {file_abspath}")
-        
-        
-            #sd = SalesData(path=cf.FILE_DATA)
-            #sd.cleaned_file(year=self.year)
+            if file != 'README.md' :
+                file_abspath = os.path.abspath(os.path.join(cf.DATA_DIR, file))
+                year = re.search(r'(\d{4})', file).group(1)
+                self.log.info(f"file_abspath : {file_abspath} year : {year}")
+                sd = SalesData(path=file_abspath)
+                sd.cleaned_file(year=year)
             
         
         

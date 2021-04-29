dc1cb2a87d1f7f0499041f13cd1f34845e8a305f
alessio rea <alexei.80@hotmail.fr>
2021-04-29 13:27:32 +0200

    progress retrieving html

--- airflow/plugins/operators/load_currency.py
+++ airflow/plugins/operators/load_currency.py
@@ -25,13 +25,27 @@ class LoadCurrencyOperator(BaseOperator) :
     def get_data_from(self, url):
         r = requests.get(url)
         soup = BeautifulSoup(r.text, 'lxml')
-        return soup.prettify()
+        li = soup.find_all('li', class_="currencylist-item")
+
+        country_names = []
+        currency_names = []
+        currency_values = []
+
+        for li in soup.find_all('li', class_="currencylist-item") :
+            country_name = li.find('span')
+            currency_name = li.find('a')
+            currency_value = li.find('strong')
+            country_names.append(country_name.text)
+            currency_names.append(currency_name.text)
+            currency_values.append(currency_value.text)
+
+        return country_names, currency_names, currency_values
 
 
     def execute(self, context):
         self.log.info(f"load currencies form  : {cf.CURRENCY_URL}")
-        data = self.get_data_from(cf.CURRENCY_URL)
-        self.log.info(f"data  : {data}")
+        country_names, currency_names, currency_values = self.get_data_from(cf.CURRENCY_URL)
+        self.log.info(f"country_name  : {country_names[0]} - currency_name  : {currency_names[0]} - currency_value  : {currency_values[0]}")
         
         
         

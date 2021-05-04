630c6a63b12c0ffb652ad337a6064aff2d345cb3
alessio rea <alexei.80@hotmail.fr>
2021-05-04 08:32:46 +0200

    progress initial-dag

--- airflow/plugins/operators/clean_file.py
+++ airflow/plugins/operators/clean_file.py
@@ -22,16 +22,16 @@ class CleanFileOperator(BaseOperator) :
 
 
     def execute(self, context):
-        item_path = os.path.abspath(os.path.join(cf.DATA_DIR, item))
+        item_path = os.path.abspath(os.path.join(cf.DATA_DIR, self.item))
         self.log.info(f"Create cleaned files in : {item_path}")
         files = [f for f in listdir(item_path) if isfile(join(item_path, f))]
         for file in files :
             if file != 'README.md' and not file.startswith('.') :
-                file_abspath = os.path.abspath(os.path.join(cf.DATA_DIR, item, file))
+                file_abspath = os.path.abspath(os.path.join(cf.DATA_DIR, self.item, file))
                 self.log.info(f"Name of files : {file}")
                 year = re.search(r'(\d{4})', file).group(1)
                 self.log.info(f"Create cleaned file : {file} for year {year}")
-                sd = UtilityClass(path=file_abspath)
+                sd = self.UtilityClass(path=file_abspath)
                 sd.cleaned_file(year=year)
             
         

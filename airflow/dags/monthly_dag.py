3323e5aed5d10f4284fcacc3420be1ed9c44f811
alessio rea <alexei.80@hotmail.fr>
2021-05-18 08:19:23 +0200

    progress monthly dag

---
+++ airflow/dags/monthly_dag.py
@@ -0,0 +1,94 @@
+from datetime import datetime, timedelta
+import os
+from os import listdir
+from os.path import isfile, join, isdir, abspath
+from pathlib import Path
+import json
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+from operators.clean_file import CleanFileOperator
+from operators.upload_file import UploadFileOperator
+from operators.load_currency import LoadCurrencyOperator
+from operators.load_cities import LoadCitiesOperator
+from operators.load_mapping_regions import LoadMappingOperator
+
+from infrastructure.SalesData import SalesData
+from infrastructure.UserData import UserData
+from infrastructure.StoreData import StoreData
+
+from helpers import SqlQueries
+
+import config.config as cf
+
+
+default_args = {
+    'owner': 'alex-toy',
+    'start_date': datetime(2020, 1, 1),
+    'end_date': datetime(2021, 12, 31),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False,
+}
+
+
+dag = DAG(
+    'monthly_dag',
+    default_args=default_args,
+    description='Clean data from original monthly file and produces cleaned files before upload to S3.',
+    schedule_interval='0 * * * *',
+    catchup=True,
+    max_active_runs=1
+)
+
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+
+#milestone_1 = DummyOperator(task_id='milestone_1',  dag=dag)
+
+
+sales_clean_file = CleanFileOperator(
+    task_id='sales_clean_file',
+    dag=dag,
+    item='sales',
+    UtilityClass=MonthlySalesData,
+)
+
+
+
+# upload_file = UploadFileOperator(
+#     task_id='upload_file',
+#     dag=dag,
+#     aws_credentials_id="aws_credentials",
+#     S3_bucket="darties"
+# )
+
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+
+
+
+
+# start_operator >> \
+# [ 
+#     load_mapping_regions, load_cities, load_currency
+# ] >> \
+# sales_clean_file >> \
+# [
+#     sales_clean_file, user_clean_file, store_clean_file
+# ] >> \
+# check_no_null_values >> \
+# upload_file >> \
+# end_operator
+
+
+
+start_operator >> \
+sales_clean_file >> \
+end_operator
---
+++ airflow/plugins/infrastructure/MonthlySalesData.py
@@ -0,0 +1,71 @@
+# -*- coding: UTF-8 -*-
+import numpy as np 
+import pandas as pd
+from pandas.api.types import is_string_dtype
+import os
+import re
+from datetime import datetime, timedelta, date
+from pathlib import Path
+
+
+import config.config as cf
+
+class MonthlySalesData :
+    """
+    cleans sales data from raw file
+    """
+
+    def __init__(self, path, sheet_name='darties') :
+        self.path = path
+        self.sheet_names = []
+
+    
+    def df_from_path(self):
+        name, extension = os.path.splitext(self.path)
+        if extension == '.csv':
+            return pd.read_csv(path)
+        elif extension == '.parquet':
+            return pd.read_parquet(path)
+        elif extension == '.xlsx':
+            df = pd.read_excel(self.path, sheet_name=None, engine='openpyxl')
+            self.sheet_names = df.keys()
+            return df
+        else:
+            raise FileExistsError('Extension must be parquet or csv or xlsx.')
+
+
+
+    def cleaned_file(self, year) :
+        df = self.df_from_path()
+        for sheet_name in self.sheet_names :
+            df_sheet_name = df[sheet_name]
+            df_sheet_name.columns = [x.lower() for x in df_sheet_name.columns]
+            df_sheet_name = cf.remove_accents(df=df_sheet_name)
+            outdir = os.path.join(cf.OUTPUTS_DIR, sheet_name, year)
+            path = Path(outdir)
+            path.mkdir(parents=True, exist_ok=True)
+            print(sheet_name)
+            print(year)
+            saved_filename = f"{sheet_name}_{year}_sales.json"
+            df_sheet_name['annee'] = year
+            df_sheet_name.to_json(os.path.join(outdir, saved_filename), orient="records", lines=True)
+
+
+
+
+
+if __name__ == '__main__':
+
+    FILE_DATA = "/Users/alexei/docs150521/MIAGE/S4/D605/darties/airflow/data/monthly_sales/Janvier_2021.xlsx"
+    
+    msd = MonthlySalesData(path=FILE_DATA)
+    msd.cleaned_file(year=2021)
+
+
+
+
+
+
+
+
+

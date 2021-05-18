15472b21e25aa17474dc7692a2060a12096f447e
alessio rea <alexei.80@hotmail.fr>
2021-05-18 10:51:27 +0200

    progress monthly dag

---
+++ airflow/dags/create_monthly_tables.sql
@@ -0,0 +1,87 @@
+CREATE TABLE IF NOT EXISTS public.staging_monthly_ca_fours (
+	villes varchar(256),
+	annee int4,
+	lib_mois varchar(30),
+	o_mois decimal(16,8),
+	r_mois decimal(16,8)
+);
+
+
+CREATE TABLE IF NOT EXISTS public.staging_monthly_v_fours (
+	villes varchar(256),
+	annee int4,
+	lib_mois varchar(30),
+	o_mois decimal(16,8),
+	r_mois decimal(16,8)
+);
+
+
+CREATE TABLE IF NOT EXISTS public.staging_monthly_mb_fours (
+	villes varchar(256),
+	annee int4,
+	lib_mois varchar(30),
+	o_mois decimal(16,8),
+	r_mois decimal(16,8)
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS public.staging_monthly_ca_hifi (
+	villes varchar(256),
+	annee int4,
+	lib_mois varchar(30),
+	o_mois decimal(16,8),
+	r_mois decimal(16,8)
+);
+
+
+CREATE TABLE IF NOT EXISTS public.staging_monthly_v_hifi (
+	villes varchar(256),
+	annee int4,
+	lib_mois varchar(30),
+	o_mois decimal(16,8),
+	r_mois decimal(16,8)
+);
+
+
+CREATE TABLE IF NOT EXISTS public.staging_monthly_mb_hifi (
+	villes varchar(256),
+	annee int4,
+	lib_mois varchar(30),
+	o_mois decimal(16,8),
+	r_mois decimal(16,8)
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS public.staging_monthly_ca_magneto (
+	villes varchar(256),
+	annee int4,
+	lib_mois varchar(30),
+	o_mois decimal(16,8),
+	r_mois decimal(16,8)
+);
+
+
+CREATE TABLE IF NOT EXISTS public.staging_monthly_v_magneto (
+	villes varchar(256),
+	annee int4,
+	lib_mois varchar(30),
+	o_mois decimal(16,8),
+	r_mois decimal(16,8)
+);
+
+
+CREATE TABLE IF NOT EXISTS public.staging_monthly_mb_magneto (
+	villes varchar(256),
+	annee int4,
+	lib_mois varchar(30),
+	o_mois decimal(16,8),
+	r_mois decimal(16,8)
+);
+
+
+
+
